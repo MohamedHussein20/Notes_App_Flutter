d18a7994c0fb3d1ca31a03263941e1fc047a7171
Mohamed Hussein <mohussein1279@gmail.com>
2025-08-14 01:01:02 +0300

    change theme and create notes screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/screens/notes_screen.dart';
 
-main(){
-
+main() {
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: NotesScreen(),
     );
   }
 }
