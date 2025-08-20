9112e0b45001979844723eef06ba268225e6485e
Mohamed Hussein <mohussein1279@gmail.com>
2025-08-21 01:42:28 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -7,15 +7,9 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
-  fetchAllNotes(){
-    try {
-      var notesBox = Hive.box<Note>("notes_box");
-      var notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+  List<Note>? notes = [];
+  fetchAllNotes() {
+    var notesBox = Hive.box<Note>("notes_box");
+    notes = notesBox.values.toList();
   }
-
 }
--- lib/widgets/notes_screen_body.dart
+++ lib/widgets/notes_screen_body.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/notes_listview.dart';
 
-class NotesScreenBody extends StatelessWidget {
+class NotesScreenBody extends StatefulWidget {
   const NotesScreenBody({super.key});
+
+  @override
+  State<NotesScreenBody> createState() => _NotesScreenBodyState();
+}
+
+class _NotesScreenBodyState extends State<NotesScreenBody> {
+@override
+void initState(){
+  
+  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+  super.initState();
+}
 
   @override
   Widget build(BuildContext context) {
