e3e9319c74ef3d710495821ec3ff080bf6f5a616
Mohamed Hussein <mohussein1279@gmail.com>
2025-08-22 03:17:15 +0300

    add note bottom sheet done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
@@ -7,7 +8,9 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color color = Color(0xffAC3931);
   addNote(Note note) async {
+    note.color = color.toARGB32();
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<Note>("notes_box");
--- lib/widgets/colors_listview.dart
+++ lib/widgets/colors_listview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/color_item.dart';
 
 class ColorsListView extends StatefulWidget {
@@ -11,16 +13,16 @@ class ColorsListView extends StatefulWidget {
 class _ColorsListViewState extends State<ColorsListView> {
   int currentIndex = 0;
   List<Color> colors = const [
-    Colors.purple,
-    Colors.red,
-    Colors.green,
-    Colors.yellow,
-    Colors.purple,
-    Colors.orange,
-    Colors.cyan,
-    Colors.pink,
-    Colors.brown,
-    Colors.indigo,
+    Color(0xffAC3931),
+    Color(0xffE5D352),
+    Color(0xffD9E76C),
+    Color(0xff537D8D),
+    Color(0xff482C3D),
+    Color(0xff8338ec),
+    Color(0xff6A994E),
+    Color(0xffA7C957),
+    Color(0xffff5400),
+    Color(0xffd90368),
   ];
   @override
   Widget build(BuildContext context) {
@@ -35,6 +37,7 @@ class _ColorsListViewState extends State<ColorsListView> {
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                 setState(() {});
               },
               child: ColorItem(isActive: currentIndex == index, color: colors[index]),
